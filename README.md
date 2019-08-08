fa15a6001bbf82f6622b70bb617d6d3e4aaccf9f
eunjung31 <ej.yeo@snu.ac.kr>
2019-08-08 16:08:38 +0900

    kaldi_tutorial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Kaldi_dummies
+
+This is a practice for using Kaldi. I have followed the instructions on kaldi for dummies.
---
+++ cmd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Setting local system jobs (local CPU - no external clusters)
+export train_cmd=run.pl
+export decode_cmd=run.pl
